59ebf64bbe948673a543d24d9f487f1887fba4f4
Shubham Sharma <shubham3786.2007@gmail.com>
2026-01-07 20:03:01 +0530

    Add program to find largest odd number in string
    Implement a program to find the largest odd number in a string of digits, handling both cases where the final number is odd or even.

---
+++ largest odd number in string.cpp
@@ -0,0 +1,51 @@
+// to find the largest odd number in the string 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    cout << "Enter a string of numbers: ";
+    cin >> s;
+    
+    int oddNum = 0;
+    int multiple = 10;
+    int len = s.length();
+    
+    for (int n = 0; n < len; n++)
+    {
+        int i = s[n] - '0';
+        cout << i << endl;
+        if (i != 0)
+        {
+            oddNum = (oddNum * multiple) + i;
+            cout << oddNum << "in odd num " << endl;
+        }
+        
+    }
+    cout << "OddNum " << oddNum << endl;
+    if (oddNum % 2 !=0)
+    {
+        cout << "The final number is: " << oddNum;
+    }
+    else 
+    {
+        int rem;
+        int midNum;
+        while (oddNum)
+        {
+            rem = oddNum % 10;
+            midNum = oddNum - rem;
+            oddNum = oddNum / 10;
+            if (oddNum % 2 != 0)
+            {
+                cout << "the final required number is:" << oddNum;
+                break;
+            }
+            else if (oddNum == 0)
+            {
+                cout << "The string do not contain any odd number";
+            }
+        }
+    }
+}
